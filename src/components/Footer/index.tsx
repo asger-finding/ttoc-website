980077c8e84daa3d395306c905dbe579e6de92dd
Asger Finding <asger.finding@gmail.com>
2022-09-10 18:47:58 +0200

    feat: transform footer depending on screen width

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -5,23 +5,28 @@ import {
 
 export default function Footer(props: {}) {
     return <div className="Footer">
-        <div className="Footer-Tanks">
+        <div style={{ gridArea: 'footerTanks' }} className="Footer-Tanks">
             <p style={{ gridArea: 'creator' }}>Created by</p>
             <div style={{ gridArea: 'creatorTank' }}>
                 <Tank playerId="29725" size={TANK_ICON_SIZES.Large} outline="false"></Tank>
             </div>
             <p style={{ gridArea: 'developer' }}>Sponsored by the TankTrouble scientists</p>
-            <div style={{ gridArea: 'developerTank', display: 'flex' }}>
+            <div style={{
+                    gridArea: 'developerTank',
+                    display: 'flex',
+                    flexWrap: 'wrap',
+                    justifyContent: 'center'
+                }}>
                 <Tank playerId="1145079" size={TANK_ICON_SIZES.Large} outline="false"></Tank>
                 <Tank playerId="160470" size={TANK_ICON_SIZES.Large} outline="false"></Tank>
             </div>
         </div>
-        <div className="Footer-Navigation">
+        <div style={{ gridArea: 'footerNavigation' }} className="Footer-Navigation">
             <p><a href="https://tanktrouble.com">Visit TankTrouble</a></p>
             <p><a href="https://github.com/asger-finding/ttoc-website">Website Source</a></p>
             <p><a href="mailto:contact@ttoc.ca">Contact Us!</a></p>
             <br></br>
-            TTOC: Powering competitive TankTrouble since 2016
+            <p className='Footer-Tagline'><b>TTOC</b>: Powering competitive TankTrouble since 2016</p>
         </div>
     </div>
 }
